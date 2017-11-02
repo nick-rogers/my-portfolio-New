7ef4925eb76ed0c12c6b1af51eed3b9fcd8605a4
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2017-11-02 18:05:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 $(document).ready(function(){
 
+var pageWidth = $(window).width();
 
+$(window).resize(function(){
+	pageWidth = $(window).width();
+});
+
+console.log(pageWidth);
 
 // When the page is scrolled the nav bar shrinks and the contained elements shift
 $(window).scroll(function(){
 	var distFromTop = $(document).scrollTop();
 	console.log(distFromTop);
-	if(distFromTop > 5){
-		$('.main').addClass('main-toggle');
-		$('.main').removeClass('main');
-		$('.top').addClass('top-shrink');
-		$('.top').removeClass('top');
-		$('.intro').fadeOut(100);
-    }
-    else {
-    	$('.main-toggle').addClass('main');
-		$('.main-toggle').removeClass('main-toggle');
-		$('.top-shrink').addClass('top');
-		$('.top-shrink').removeClass('top-shrink');
-		$('.intro').fadeIn(300);
-    }
+	if(pageWidth > 667){
+		
+		if(distFromTop > 5){
+			$('.main').addClass('main-toggle');
+			$('.main').removeClass('main');
+			$('.top').addClass('top-shrink');
+			$('.top').removeClass('top');
+			$('.intro').fadeOut(100);
+			$('.logo').fadeIn(100);
+	    }
+	    else {
+	    	$('.main-toggle').addClass('main');
+			$('.main-toggle').removeClass('main-toggle');
+			$('.top-shrink').addClass('top');
+			$('.top-shrink').removeClass('top-shrink');
+			$('.intro').fadeIn(300);
+			$('.logo').fadeOut(100);
+	    }
+	}
 });
 
 
